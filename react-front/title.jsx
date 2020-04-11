70cfba9cdcc1b1d9c187e6fdde0c666d148ce9f5
GX-Freedom <kjy911206@gmail.com>
2020-04-11 13:31:48 +0900

    update
    update

---
+++ react-front/title.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BaseLayout } from "./globalStyles/layout";
+import styled, { createGlobalStyle } from "styled-components";
+import GlobalStyle from "./globalStyles/ResetCss";
+
+
+
+
+export default function title() {
+    return (
+        <BaseLayout>
+            <GlobalStyle/>
+  
+        </BaseLayout>
+    );
+
+}
+// /images/Long_Story.png
